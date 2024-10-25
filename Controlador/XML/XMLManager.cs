360b17f0de06a15e125a9764f24ac27ea1f8897d
msanchezelv <msanchezelv@uoc.edu>
2024-10-25 22:48:54 +0200

    Revert "XML eliminado"
    This reverts commit ad3d6a28220f4d9845c6078859552f4443a2e6a4.

---
+++ Controlador/XML/XMLManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+public class XMLManager
+{
+    private string connectionString;
+    private string xmlDirectoryPath;
+
+    public XMLManager(string connectionString, string xmlDirectoryPath)
+    {
+        this.connectionString = connectionString;
+        this.xmlDirectoryPath = xmlDirectoryPath;
+    }
+    
+    // Método para generar XML de Usuarios
+    public void GenerarXMLUsuarios()
+    {
+        string query = "SELECT * FROM Usuario";
+        XElement usuariosXml = new XElement("Usuarios");
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                XElement usuario = new XElement("Usuario",
+                    new XElement("idUsuario", reader["idUsuario"]),
+                    new XElement("nombre", reader["nombre"]),
+                    new XElement("apellido", reader["apellido"]),
+                    new XElement("email", reader["email"]),
+                    new XElement("contraseña", reader["contraseña"]),
+                    new XElement("rol", reader["rol"])
+                );
+                usuariosXml.Add(usuario);
+            }
+        }
+
+        usuariosXml.Save(Path.Combine(xmlDirectoryPath, "Usuarios.xml"));
+    }
+
+    // Otros métodos para generar XML de Actividades, Reservas, Horarios, etc.
+}
