aa0e22d14c1485551be070869b511a314543c88d
msanchezelv <msanchezelv@uoc.edu>
2024-12-10 14:55:34 +0100

    importar horario, medio arreglado para Odoo

--- Controlador/XML/horario_import.py
+++ Controlador/XML/horario_import.py
@@ -1,4 +1,9 @@
-﻿# Función para leer el XML y obtener los datos de Horario
+﻿import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+# Función para leer el XML y obtener los datos de Horario
 def leer_xml_horario(xml_file):
     import xml.etree.ElementTree as ET
 
@@ -34,19 +39,20 @@ def crear_horario_en_odoo(horarios):
     for horario in horarios:
         try:
             horario_data = {
-                'day_of_week': horario['diaSemana'],
-                'start_time': horario['horaInicio'],
-                'end_time': horario['horaFin'],
-                'activity_id': horario['idActividad'],
-                'room': horario['sala'],
-                'monitor': horario['Monitor'],
-                'duration': horario['duracion']
+                'x_idhorario': horario['idHorario'],
+                'x_diasemana': horario['diaSemana'],
+                'x_horainicio': horario['horaInicio'],
+                'x_horafin': horario['horaFin'],
+                'x_idactividad': horario['idActividad'],
+                'x_sala': horario['sala'],
+                'x_monitor': horario['Monitor'],
+                'x_duracion': horario['duracion']
             }
 
             # Crear el horario en Odoo
             horario_id = models.execute_kw(
                 ODOO_CONFIG['db'], uid, ODOO_CONFIG['password'],
-                'gentefit.horario', 'create', [horario_data]
+                'x_horario', 'create', [horario_data]
             )
             print(f"Horario creado en Odoo con ID: {horario_id}")
         except Exception as e:
